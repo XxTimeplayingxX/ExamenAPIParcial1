d32d6b70ad83d5ec89ee5f7519ed0948bc8334a9
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2024-01-19 20:50:13 -0500

    Agregar archivos de proyecto.

---
+++ ExamenAPIParcial1/Controllers/ClientesController.cs
@@ -0,0 +1,55 @@
+﻿using ExamenAPIParcial1.Modelos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamenAPIParcial1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private static List<Clientes> _clientes = new List<Clientes>
+        {
+            new Clientes
+            {
+                NumeroDeCuenta = "9087789610",
+                Nombre = "David Sánchez",
+                TipoCuenta = TipoDeCuenta.Ahorro,
+                Saldo = 500
+            },
+            new Clientes
+            {
+                NumeroDeCuenta = "90872345780",
+                Nombre = "Daniel Olivo",
+                TipoCuenta = TipoDeCuenta.Corriente,
+                Saldo = 500
+            },
+            new Clientes
+            {
+                NumeroDeCuenta = "90223311310",
+                Nombre = "Edwin Falconez",
+                TipoCuenta = TipoDeCuenta.Crédito,
+                Saldo = 500
+            },
+            new Clientes
+            {
+                NumeroDeCuenta = "12135090113",
+                Nombre = "Carlos Dimitrakis",
+                TipoCuenta = TipoDeCuenta.Ahorro,
+                Saldo = 500
+            },
+            new Clientes
+            {
+                NumeroDeCuenta = "87764514093",
+                Nombre = "Betsabe Correo",
+                TipoCuenta = TipoDeCuenta.Crédito,
+                Saldo = 500
+            }
+        };
+        [HttpGet]
+        public IEnumerable<Clientes> GetAllClients()
+        {
+            return _clientes;
+        }
+    }
+}
---
+++ ExamenAPIParcial1/Modelos/Clientes.cs
@@ -0,0 +1,17 @@
+﻿namespace ExamenAPIParcial1.Modelos
+{
+    public class Clientes
+    {
+        public string NumeroDeCuenta { get; set; }
+        public string Nombre { get; set; }
+        public TipoDeCuenta TipoCuenta { get; set; }
+        public double Saldo { get; set; }
+    }
+    
+    public enum TipoDeCuenta
+    {
+        Corriente,
+        Ahorro,
+        Crédito
+    }
+}
